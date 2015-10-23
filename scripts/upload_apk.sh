8118801cd461837872491ccded5c5bb2ee9867ff
Eric Tse <etse@topps.com>
2015-10-23 17:49:38 -0400

    Fixing upload script

--- scripts/upload_apk.sh
+++ scripts/upload_apk.sh
@@ -3,9 +3,9 @@
 # Upload to HockeyApp
 curl https://rink.hockeyapp.net/api/2/apps/$HOCKEY_APP_ID/app_versions \
   -F status="2" \
-  -F notify="0" \
-  -F notes="Build!11" \
+  -F notify="1" \
+  -F notes="Some kind of build note" \
   -F notes_type="0" \
-  -F ipa="../app/build/outputs/app-debug.apk" \
+  -F ipa="@../app/build/outputs/app-debug.apk" \
 #  -F dsym="@$OUTPUTDIR/$APP_NAME.app.dSYM.zip" \
   -H "X-HockeyAppToken: $HOCKEY_APP_TOKEN"
