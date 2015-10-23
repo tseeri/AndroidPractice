077c696f24e2e35655cf631b7bd86adc463cdb0d
Eric Tse <etse@topps.com>
2015-10-23 17:58:02 -0400

    Fixing upload script binary path, removing commented line

--- scripts/upload_apk.sh
+++ scripts/upload_apk.sh
@@ -6,6 +6,5 @@ curl https://rink.hockeyapp.net/api/2/apps/$HOCKEY_APP_ID/app_versions \
   -F notify="1" \
   -F notes="Some kind of build note" \
   -F notes_type="0" \
-  -F ipa="@../app/build/outputs/app-debug.apk" \
-#  -F dsym="@$OUTPUTDIR/$APP_NAME.app.dSYM.zip" \
+  -F ipa="@../app/build/outputs/apk/app-debug.apk" \
   -H "X-HockeyAppToken: $HOCKEY_APP_TOKEN"
