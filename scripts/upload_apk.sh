6b8c24e16be986f41a569e1763f2f6e80b7bf8c8
Eric Tse <etse@topps.com>
2015-10-23 17:31:03 -0400

    fixing upload script

--- scripts/upload_apk.sh
+++ scripts/upload_apk.sh
@@ -6,6 +6,6 @@ curl https://rink.hockeyapp.net/api/2/apps/$HOCKEY_APP_ID/app_versions \
   -F notify="0" \
   -F notes="Build!11" \
   -F notes_type="0" \
-  -F ipa="../app/build/outputs/apk.ipa" \
+  -F ipa="../app/build/outputs/app-debug.apk" \
 #  -F dsym="@$OUTPUTDIR/$APP_NAME.app.dSYM.zip" \
   -H "X-HockeyAppToken: $HOCKEY_APP_TOKEN"
