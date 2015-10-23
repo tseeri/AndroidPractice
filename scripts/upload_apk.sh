f0319ebcde7579377f2ec7ad9b9e8174dc532200
Eric Tse <etse@topps.com>
2015-10-23 18:13:03 -0400

    Fixing upload scripts - trying quotes around encrypted data

--- scripts/upload_apk.sh
+++ scripts/upload_apk.sh
@@ -7,4 +7,4 @@ curl https://rink.hockeyapp.net/api/2/apps/$HOCKEY_APP_ID/app_versions \
   -F notes="Some kind of build note" \
   -F notes_type="0" \
   -F ipa="@../app/build/outputs/apk/app-debug.apk" \
-  -H "X-HockeyAppToken: $HOCKEY_APP_TOKEN"
+  -H "X-HockeyAppToken: $HOCKEY_APP_TOKEN" ; echo
