8ab6debc41df4cbf086a705ac61bc773b8be78c0
Eric Tse <etse@topps.com>
2015-10-26 12:24:33 -0400

    Getting better logging for debugging travis-hockeyapp issues

--- scripts/upload_apk.sh
+++ scripts/upload_apk.sh
@@ -2,15 +2,13 @@
 
 printf "Uploading to HockeyApp...\n"
 
-set -v
-
 # Upload to HockeyApp
 uploadResponse=$(curl https://rink.hockeyapp.net/api/2/apps/$HOCKEY_APP_ID/app_versions \
   -F status="2" \
   -F notify="1" \
-  -F notes="Build note" \
+  -F notes="Travis" \
   -F notes_type="0" \
   -F ipa="@app/build/outputs/apk/app-release-unsigned.apk" \
   -H "X-HockeyAppToken: $HOCKEY_APP_TOKEN")
 
-printf $uploadResponse
+echo $uploadResponse
