d2a3fe2625cb8a9283436611b72749dcffd22e12
Eric Tse <etse@topps.com>
2015-10-26 11:09:02 -0400

    Attempted fix for hockeyapp upload - redoing app ID and token encryption

--- scripts/upload_apk.sh
+++ scripts/upload_apk.sh
@@ -2,15 +2,13 @@
 
 printf "Uploading to HockeyApp...\n"
 
-set -v
-
 # Upload to HockeyApp
-uploadResponse=$(curl https://rink.hockeyapp.net/api/2/apps/${HOCKEY_APP_ID}/app_versions \
+uploadResponse=$(curl https://rink.hockeyapp.net/api/2/apps/$HOCKEY_APP_ID/app_versions \
   -F status="2" \
   -F notify="1" \
   -F notes="Some kind of build note" \
   -F notes_type="0" \
   -F ipa="@../app/build/outputs/apk/app-debug.apk" \
-  -H "X-HockeyAppToken: ${HOCKEY_APP_TOKEN}")
+  -H "X-HockeyAppToken: $HOCKEY_APP_TOKEN")
 
 printf $uploadResponse
