e9493f714475941cbc5de9c9c85fbba754bdcef3
Eric Tse <etse@topps.com>
2015-10-26 11:39:25 -0400

    Hockeyapp upload fix attempt - fix app name and path to apk

--- scripts/upload_apk.sh
+++ scripts/upload_apk.sh
@@ -6,9 +6,9 @@ printf "Uploading to HockeyApp...\n"
 uploadResponse=$(curl https://rink.hockeyapp.net/api/2/apps/$HOCKEY_APP_ID/app_versions \
   -F status="2" \
   -F notify="1" \
-  -F notes="Some kind of build note" \
+  -F notes="Build note" \
   -F notes_type="0" \
-  -F ipa="@../app/build/outputs/apk/app-debug.apk" \
+  -F ipa="@app/build/outputs/apk/app-release-unsigned.apk" \
   -H "X-HockeyAppToken: $HOCKEY_APP_TOKEN")
 
 printf $uploadResponse
