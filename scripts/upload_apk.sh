6980559b2f65aeea45eb72444c89b2434c82deaf
Eric Tse <etse@topps.com>
2015-10-26 14:27:27 -0400

    Adding release notes, removing debug prints

--- scripts/upload_apk.sh
+++ scripts/upload_apk.sh
@@ -2,13 +2,14 @@
 
 printf "Uploading to HockeyApp...\n"
 
+RELEASE_DATE=`date '+%Y-%m-%d %H:%M:%S'`
+RELEASE_NOTES="Build: $TRAVIS_BUILD_NUMBER\nUploaded: $RELEASE_DATE"
+
 # Upload to HockeyApp
-uploadResponse=$(curl https://rink.hockeyapp.net/api/2/apps/$HOCKEY_APP_ID/app_versions \
+curl https://rink.hockeyapp.net/api/2/apps/$HOCKEY_APP_ID/app_versions \
   -F status="2" \
   -F notify="1" \
-  -F notes="Travis" \
+  -F notes="$RELEASE_NOTES" \
   -F notes_type="0" \
   -F ipa="@app/build/outputs/apk/app-release-unsigned.apk" \
-  -H "X-HockeyAppToken: $HOCKEY_APP_TOKEN")
-
-echo $uploadResponse
+  -H "X-HockeyAppToken: $HOCKEY_APP_TOKEN"
